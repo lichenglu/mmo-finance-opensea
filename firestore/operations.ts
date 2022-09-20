2678be312905fd18614523ece61f84e108e44a46
Chenglu Li <kabelee92@gmail.com>
2022-09-19 22:29:47 -0500

    Add cronjob to poll advanced offers

--- firestore/operations.ts
+++ firestore/operations.ts
@@ -16,3 +16,32 @@ export async function addNFTItems(items: NFTAsset[]) {
     })
   )
 }
+
+export async function getAdvancedOffers() {
+  const offerRef = db.collection('OpenSeaPudgyPenguinsAdvancedOffers')
+  const snapshot = await offerRef.get()
+  const documents = {};
+    snapshot.forEach(doc => {
+      documents[doc.id] = doc.data()
+  })
+  return documents
+}
+
+export async function removeAdvancedOffers(offers: any[]) {
+  for (const offer of offers) {
+    const offerRef = db.collection('OpenSeaPudgyPenguinsAdvancedOffers').doc(offer.order_hash)
+    await offerRef.delete()
+  }
+}
+
+export async function setAdvancedOffers(offers: any[]) {
+  for (const offer of offers) {
+    const offerRef = db.collection('OpenSeaPudgyPenguinsAdvancedOffers').doc(offer.order_hash)
+    await offerRef.set(offer, { merge: true })
+  }
+}
+
+export async function testFSConnection() {
+    const ref = db.collection('testing').doc((new Date()).toString())
+    await ref.set({ time: new Date() + ' hello' }, { merge: true })
+}
--- index.ts
+++ index.ts
@@ -2,8 +2,13 @@ import express = require('express')
 import { CronJob } from 'cron'
 import { OpenSeaStreamClient } from '@opensea/stream-js'
 import { WebSocket } from 'ws'
+import { create } from 'apisauce'
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
 
 import { getAssetsExaustively, updateToken } from './utils'
+import { testFSConnection, getAdvancedOffers, setAdvancedOffers, removeAdvancedOffers } from './firestore/operations'
 
 const PORT = process.env.PORT || 8080
 const app = express()
@@ -11,6 +16,9 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('🎉 Hello World! 🎉')
 })
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
 
 const server = app.listen(PORT, async () => {
   console.log(`App listening on port ${PORT}`)
@@ -31,6 +39,54 @@ const server = app.listen(PORT, async () => {
   //   'America/Chicago'
   // );
 
+  const api = create({
+    baseURL: `https://api.opensea.io`,
+    headers: { 'X-API-KEY': 'e8aafbf2081c4489a5ae3539a47d82f3' },
+  })
+
+  const openseaOfferJob = new CronJob(
+    '*/60 * * * * *',
+    async function() {
+      try {
+        console.log('Running job...getting assets');
+        const tokenAddress = '0xbd3531da5cf5857e7cfaa92426877b022e612cf8'
+  
+        const res = await api.get(`/api/v1/asset/${tokenAddress}/0/offers`)
+        if (res.ok) {
+          // @ts-ignore
+          const advancedOffers = res.data.seaport_offers.filter((bid: any) => bid.side === 'bid' && bid.order_type === 'criteria')
+          const currentOffers = await getAdvancedOffers()
+          const now = dayjs(undefined, { utc: true, })
+          const newOffers = advancedOffers.filter((offer: any) => !currentOffers[offer.order_hash])
+          newOffers.forEach(offer => {
+            currentOffers[offer.order_hash] = offer
+          }) 
+          const updatedOffers = []
+          const offersToBeRemoved = []
+          Object.entries(currentOffers).forEach(([key, value]: [any, any]) => {
+            if (now.isBefore(dayjs.utc(value.closing_date))) {
+              updatedOffers.push(value)
+            } else {
+              offersToBeRemoved.push(value)
+            }
+          })
+          await removeAdvancedOffers(offersToBeRemoved);
+          await setAdvancedOffers(updatedOffers)
+          console.log(`Completed ${new Date()}`)
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    function() {
+      console.log(`Completed ${new Date()}`)
+    },
+    true,
+    'America/Chicago'
+  );
+
+  testFSConnection()
+  
   const client = new OpenSeaStreamClient({
     token: 'e8aafbf2081c4489a5ae3539a47d82f3',
     connectOptions: {
